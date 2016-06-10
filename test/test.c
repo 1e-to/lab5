487e9303655947cf3161a22a86510f724f6e1032
totoro4 <totmeninal@mail.ru>
2016-06-10 20:53:02 +0600

    Added test for two roots

---
+++ test/test.c
@@ -0,0 +1,26 @@
+#include <ctest.h>
+#include <roots.h>
+
+CTEST(equation_suite, two_roots_test)
+{
+    // Given
+    const double a = 1;
+    const double b = 3;
+    const double c = 2;
+
+    // When
+    double x1;
+    double x2;
+    int amount;
+    roots(a, b, c, &amount, &x1, &x2);
+
+    // Then
+    
+    const double expected_amount = 2;
+    const double expected_x1 = -1;
+    const double expected_x2 = -2;
+
+    ASSERT_EQUAL(expected_amount, amount);
+    ASSERT_DBL_NEAR(expected_x1, x1);
+    ASSERT_DBL_NEAR(expected_x2, x2);
+}
