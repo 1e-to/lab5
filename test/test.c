b6e7426e663aae3d1dedf2f83c6d6cff707be416
totoro4 <totmeninal@mail.ru>
2016-06-10 20:55:49 +0600

    Added test for one root

--- test/test.c
+++ test/test.c
@@ -24,3 +24,24 @@ CTEST(equation_suite, two_roots_test)
     ASSERT_DBL_NEAR(expected_x1, x1);
     ASSERT_DBL_NEAR(expected_x2, x2);
 }
+
+CTEST(equation_suite, one_root_test)
+{
+    // Given
+    const double a = 2;
+    const double b = -4;
+    const double c = 2;
+
+    // When
+    double x1;
+    double x2;
+    int amount;
+    roots(a, b, c, &amount, &x1, &x2);
+
+    // Then
+    const double expected_amount = 1;
+    const double expected_x1 = 1;
+
+    ASSERT_EQUAL(expected_amount, amount);
+    ASSERT_DBL_NEAR(expected_x1, x1);
+}
